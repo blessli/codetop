89b28db73fe4d09cb7af7dcf139551e1a5df354b
blessli <ldm112358@163.com>
2023-01-19 03:30:03 +0800

    18号3道

--- 20230118.go
+++ 20230118.go
@@ -2,6 +2,78 @@ package main
 
 import "sort"
 
+func canJump(nums []int) bool {
+	n := len(nums)
+	if n == 1 {
+		return true
+	}
+	m := n - 1
+	for i := n - 2; i >= 0; i-- {
+		if nums[i]+i >= m {
+			m = i
+		}
+	}
+	return m == 0
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	pre := map[int]*TreeNode{}
+	var dfs func(*TreeNode)
+	dfs = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+		if root.Left != nil {
+			pre[root.Left.Val] = root
+		}
+		if root.Right != nil {
+			pre[root.Right.Val] = root
+		}
+		dfs(root.Left)
+		dfs(root.Right)
+	}
+	dfs(root)
+	vis := map[int]bool{}
+	for p != nil {
+		vis[p.Val] = true
+		p = pre[p.Val]
+	}
+	for q != nil {
+		if vis[q.Val] {
+			return q
+		}
+		q = pre[q.Val]
+	}
+	return nil
+}
+
+// 搜索旋转排序数组
+func search1(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+	for left <= right {
+		mid := left + (right-left)/2
+		v := nums[mid]
+		if v == target {
+			return mid
+		}
+		if v > target {
+			if target < nums[left] && v > nums[right] {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else {
+			if target > nums[right] && v < nums[left] {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+		}
+	}
+	return -1
+}
+
 func permute(nums []int) [][]int {
 	n := len(nums)
 	vis := make([]int, n)
